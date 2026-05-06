16f128fd37c1a85f65ee9684135c00d05e54fea4
coding1100 <coding1343453@gmail.com>
2026-05-06 15:48:33 +0500

    gtm script added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Suspense } from "react";
+import Script from "next/script";
 import {
   Bricolage_Grotesque,
   Alegreya_Sans,
@@ -129,6 +130,17 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <head>
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-W29MGBSS');`,
+          }}
+        />
         {fontsWithImports.map((font: EditorFontOption, index: number) => (
           <link
             key={index}
@@ -140,6 +152,14 @@ export default async function RootLayout({
       <body
         className={`${bodyFont.variable} ${headingFont.variable} ${displayFont.variable} ${alegreyaSansFont.variable} ${poiretFont.variable} ${interFont.variable} ${cormorantFont.variable} ${libreBaskervilleFont.variable} ${latoFont.variable} antialiased`}
       >
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-W29MGBSS"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <Suspense fallback={null}>
           <TopLoader />
         </Suspense>
